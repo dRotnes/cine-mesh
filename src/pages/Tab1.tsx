c6ce4df712ae8dce280f4b053d3ae5528e396e8e
dRotnes <rotnesdante10@gmail.com>
2023-09-27 15:32:40 +0100

    commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -8,7 +8,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Tab 5</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
